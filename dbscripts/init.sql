0deb44dc5dac588594e0e254dbb3b9589b133d9a
jianminglok <jianminglok@hotmail.com>
2023-06-11 17:46:18 +0800

    Add DB initialization script

---
+++ dbscripts/init.sql
@@ -0,0 +1,9 @@
+use im;
+CREATE TABLE `messages` (
+	ID INT NOT NULL AUTO_INCREMENT,
+    chat varchar(255) NOT NULL,
+    text varchar(255) NOT NULL,
+    sender varchar(255) NOT NULL,
+    sendtime BIGINT NOT NULL,
+    PRIMARY KEY (ID)
+)
--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -21,7 +21,7 @@ const sendQuery = "INSERT INTO `messages` (`chat`, `text`, `sender`, `sendtime`)
 
 func init() {
 	var err error
-	db, err = sql.Open("mysql", "root:mauFJcuf5dhRMQrjj@tcp(db:3306)/im")
+	db, err = sql.Open("mysql", "root:rootPasswordToBeUpdated@tcp(db:3306)/im")
 	if err != nil {
 		log.Fatalf("Error establishing connection to database: %s", err)
 	}
@@ -93,55 +93,56 @@ func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.Pu
 		resp.Messages = append(resp.Messages, &message)
 	}
 
-	if &req.Reverse != nil && *req.Reverse == true {
-		for i, j := 0, len(resp.Messages)-1; i < j; i, j = i+1, j-1 {
-			resp.Messages[i], resp.Messages[j] = resp.Messages[j], resp.Messages[i]
-		}
-	}
+	if len(resp.Messages) > 0 {
+		if &req.Reverse != nil && *req.Reverse == true {
+			for i, j := 0, len(resp.Messages)-1; i < j; i, j = i+1, j-1 {
+				resp.Messages[i], resp.Messages[j] = resp.Messages[j], resp.Messages[i]
+			}
 
-	if &req.Reverse != nil && *req.Reverse == true {
-		var testStatement = "SELECT sendtime from `messages` WHERE `chat` = (?) AND `sendtime` < (?) ORDER BY `sendtime` DESC"
-		if &req.Limit != nil && req.Limit > 0 {
-			testStatement += " LIMIT " + fmt.Sprint(req.Limit)
+			var testStatement = "SELECT sendtime from `messages` WHERE `chat` = (?) AND `sendtime` < (?) ORDER BY `sendtime` DESC"
+			if &req.Limit != nil && req.Limit > 0 {
+				testStatement += " LIMIT " + fmt.Sprint(req.Limit)
+			} else {
+				pullStatement += " LIMIT 10"
+			}
+
+			resultsOther, err := db.Query(testStatement, req.Chat, resp.Messages[len(resp.Messages)-1].SendTime)
+			if err != nil && err != sql.ErrNoRows {
+				log.Printf("Error retrieving next cursor from database: %s", err)
+				resp.Code, resp.Msg = consts.StatusInternalServerError, "Error retrieving messages"
+				return resp, nil
+			} else if err == sql.ErrNoRows {
+				hasMore = false
+			} else {
+				for resultsOther.Next() {
+					err := resultsOther.Scan(&nextCursor)
+					if err != nil {
+						if err == sql.ErrNoRows {
+							hasMore = false
+						}
+						log.Fatal(err)
+					} else {
+						hasMore = true
+						resp.NextCursor = &nextCursor
+					}
+				}
+			}
 		} else {
-			pullStatement += " LIMIT 10"
-		}
-		resultsOther, err := db.Query(testStatement, req.Chat, resp.Messages[len(resp.Messages)-1].SendTime)
-		if err != nil && err != sql.ErrNoRows {
-			log.Printf("Error retrieving next cursor from database: %s", err)
-			resp.Code, resp.Msg = consts.StatusInternalServerError, "Error retrieving messages"
-			return resp, nil
-		} else if err == sql.ErrNoRows {
-			hasMore = false
-		} else {
-			for resultsOther.Next() {
-				err := resultsOther.Scan(&nextCursor)
+			result := db.QueryRow("SELECT sendtime from `messages` WHERE `chat` = (?) AND `sendtime` > (?) ORDER BY `sendtime` ASC", req.Chat, resp.Messages[len(resp.Messages)-1].SendTime)
+			if result != nil {
+				err := result.Scan(&nextCursor)
 				if err != nil {
 					if err == sql.ErrNoRows {
 						hasMore = false
+					} else {
+						log.Printf("Error retrieving next cursor from database: %s", err)
+						resp.Code, resp.Msg = consts.StatusInternalServerError, "Error retrieving messages"
+						return resp, nil
 					}
-					log.Fatal(err)
 				} else {
 					hasMore = true
 					resp.NextCursor = &nextCursor
 				}
-			}
-		}
-	} else {
-		result := db.QueryRow("SELECT sendtime from `messages` WHERE `chat` = (?) AND `sendtime` > (?) ORDER BY `sendtime` ASC", req.Chat, resp.Messages[len(resp.Messages)-1].SendTime)
-		if result != nil {
-			err := result.Scan(&nextCursor)
-			if err != nil {
-				if err == sql.ErrNoRows {
-					hasMore = false
-				} else {
-					log.Printf("Error retrieving next cursor from database: %s", err)
-					resp.Code, resp.Msg = consts.StatusInternalServerError, "Error retrieving messages"
-					return resp, nil
-				}
-			} else {
-				hasMore = true
-				resp.NextCursor = &nextCursor
 			}
 		}
 	}

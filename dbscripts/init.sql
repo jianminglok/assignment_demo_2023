383cbb67c89f815f612ce1f3da56fd15d077bea5
jianminglok <jianminglok@hotmail.com>
2023-06-13 17:01:44 +0800

    Fix error in field type

--- dbscripts/init.sql
+++ dbscripts/init.sql
@@ -2,7 +2,7 @@ use im;
 CREATE TABLE `messages` (
 	ID INT NOT NULL AUTO_INCREMENT,
     chat varchar(255) NOT NULL,
-    text varchar(65535) NOT NULL,
+    text TEXT NOT NULL,
     sender varchar(255) NOT NULL,
     sendtime BIGINT NOT NULL,
     PRIMARY KEY (ID)
